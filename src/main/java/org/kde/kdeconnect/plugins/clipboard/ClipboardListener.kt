f11eaec5d525024d2ac051fef34d135a0d9f2803
huzzyz <69323208+huzzyz@users.noreply.github.com>
2026-09-12 13:11:31 +0400

    Restore clipboard permission imports after rebase

--- src/main/java/org/kde/kdeconnect/plugins/clipboard/ClipboardListener.kt
+++ src/main/java/org/kde/kdeconnect/plugins/clipboard/ClipboardListener.kt
@@ -6,10 +6,12 @@
 */
 package org.kde.kdeconnect.plugins.clipboard
 
+import android.Manifest
 import android.content.ClipData
 import android.content.ClipDescription
 import android.content.ClipboardManager
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
